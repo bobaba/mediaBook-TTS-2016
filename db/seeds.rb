8f989c8959af6cb352dec7229acc1283afb6c1a6
Matthew Beebe <grow.food.everywhere@gmail.com>
2016-12-20 12:24:48 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-PublicActivity::ORM::ActiveRecord::Activity.create!([
-  {trackable_type: "Friendship", trackable_id: 13, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 6},
-  {trackable_type: "Friendship", trackable_id: 13, owner_type: "User", owner_id: 6, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
-  {trackable_type: "Friendship", trackable_id: 15, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 4},
-  {trackable_type: "Friendship", trackable_id: 15, owner_type: "User", owner_id: 4, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
-  {trackable_type: "Post", trackable_id: 1, owner_type: "User", owner_id: 1, key: "post.created", parameters: {}, recipient_type: nil, recipient_id: nil}
-])
+
 User.create!([
   {email: "grow.food.everywhere@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 18, current_sign_in_at: "2016-12-20 15:41:13", last_sign_in_at: "2016-12-19 18:02:08", current_sign_in_ip: "::1", last_sign_in_ip: "::1", username: "BobabaBeebe", name: "Matthew", age: 28, bio: "I am the bestest designer ever! hoorAY! and here is some more information!", school: "Tech Talent South", work: "Caribou Cofffee"},
   {email: "student0@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "awesomeStudent0", name: nil, age: nil, bio: nil, school: nil, work: nil},
