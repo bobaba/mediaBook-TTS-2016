d7d213b3b189c0421bbda18506ee1d7252ee57f2
Matthew Beebe <grow.food.everywhere@gmail.com>
2016-12-20 11:38:29 -0500

    first commit

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,10 @@
+class ActivitiesController < ApplicationController
+before_action :authenticate_user!
+
+	def index
+		@users = current_user.active_friends
+		@users.push(current_user)
+		@activities = PublicActivity::Activity.where(owner_id: @users).order('created_at DESC')
+	end
+
+end
